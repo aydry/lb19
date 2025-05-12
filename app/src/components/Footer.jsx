32ccfe341297d15c1144e845c10c1578caadfb5d
aydry <aydry200511roblox@gmail.com>
2025-05-12 08:59:40 +0300

    1

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => (
     <footer>
-      <p>Зв’язок: <a href="https://github.com/yourgithub">GitHub</a> | <a href="mailto:youremail@gmail.com">Email</a></p>
+      <p>Зв’язок: <a href="">GitHub</a> | <a href="">Email</a></p>
     </footer>
   );
   
